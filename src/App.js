b93ec18b408e5ca93eb5983c5e908e30dec093a3
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-01 16:41:41 +0000

    fix shit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "./App.css";
-import Sidebar from "./Sidebar";
-
+import "./App.css"; 
+import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="app">
-      <h1>Hello Guys🚀 </h1>
+      <h1>Hello World🚀 🔥  </h1>
 
       {/* Sidebar */} 
+      <Sidebar/>
 
       {/* Feed */}
 
