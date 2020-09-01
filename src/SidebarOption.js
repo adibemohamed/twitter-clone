c1052391018c9a9bb4c6404671b34c4be3a67dbd
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-01 16:47:33 +0000

    add Sidebar option compo

---
+++ src/SidebarOption.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarOption() {
+    return (
+        <div className="sidbarOption">
+            <h2>This is an option</h2>
+        </div>
+    )
+}
+
+export default SidebarOption
