37805a9a6f524eb3e1cc79af1c0ab97323b252aa
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-01 22:40:46 +0000

    add embed twitter tweets + share

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,37 @@
  import React from 'react'
  import './Widgets.css';
+ import {
+     TwitterTimelineEmbed,
+     TwitterShareButton, 
+     TwitterTweetEmbed,
+ } from 'react-twitter-embed';
+
+ import SearchIcon from '@material-ui/icons/Search';
 
  function Widgets() {
      return (
          <div className="widgets">
-             <h2>Widgets</h2>
+             <div className="widgets__input">
+                 <SearchIcon className="widgets__searchIcon" />
+                 <input placeholder="Search Twitter" type="text"/>
+             </div> 
+             <div className="widgets__widgetContainer">
+                 <h2>What's happening?</h2>
+                 
+                 <TwitterTweetEmbed tweetId={"1300913237869752321"} />
+
+                 <TwitterTimelineEmbed
+                 sourceType="profile"
+                 screenMode="nasa"
+                 options={{ height: 400 }}   
+                 />
+
+                 <TwitterShareButton 
+                 url={"https://facebook.com/celeverazi"}
+                 options={{ text: "#react is awesome", via:"cleaver" }}
+                 />
+
+             </div>
          </div>
      )
  }
