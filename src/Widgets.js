f427bdc28a3bc75951aac4c840e94ea6231c1592
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-01 22:46:36 +0000

    styling embed widget

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,40 +1,39 @@
- import React from 'react'
- import './Widgets.css';
- import {
-     TwitterTimelineEmbed,
-     TwitterShareButton, 
-     TwitterTweetEmbed,
- } from 'react-twitter-embed';
+import React from 'react'
+import './Widgets.css';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from 'react-twitter-embed';
 
- import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search';
 
- function Widgets() {
-     return (
-         <div className="widgets">
-             <div className="widgets__input">
-                 <SearchIcon className="widgets__searchIcon" />
-                 <input placeholder="Search Twitter" type="text"/>
-             </div> 
-             <div className="widgets__widgetContainer">
-                 <h2>What's happening?</h2>
-                 
-                 <TwitterTweetEmbed tweetId={"1300913237869752321"} />
+function Widgets() {
+    return (
+        <div className="widgets">
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>What's happening?</h2>
 
-                 <TwitterTimelineEmbed
-                 sourceType="profile"
-                 screenMode="nasa"
-                 options={{ height: 400 }}   
-                 />
+                <TwitterTweetEmbed tweetId={"1300913237869752321"} />
 
-                 <TwitterShareButton 
-                 url={"https://facebook.com/celeverazi"}
-                 options={{ text: "#react is awesome", via:"cleaver" }}
-                 />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenMode="nasa"
+                    options={{ height: 400 }}
+                />
 
-             </div>
-         </div>
-     )
- }
- 
- export default Widgets
- 
+                <TwitterShareButton
+                    url={"https://facebook.com/celeverazi"}
+                    options={{ text: "#react is awesome", via: "cleaver" }}
+                />
+
+            </div>
+        </div>
+    )
+}
+
+export default Widgets
