7f7ba05475631269aac6e109578bc5a4a23447b2
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-10-20 16:46:18 +0000

    remove key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,6 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD1benT4jrLH_oeK4n9twLd5681GKLV85I",
-    authDomain: "twitter-clone-d4fca.firebaseapp.com",
-    databaseURL: "https://twitter-clone-d4fca.firebaseio.com",
-    projectId: "twitter-clone-d4fca",
-    storageBucket: "twitter-clone-d4fca.appspot.com",
-    messagingSenderId: "679559474951",
-    appId: "1:679559474951:web:84123ca65f01b898295fa2",
-    measurementId: "G-SZM0BVZNPB"
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
